1dd9957c3dfa55ed3cc007051b3dd09c05b42fb7
Jesus Maldonado <jmaldona18@cuc.edu.co>
2024-09-25 11:37:00 -0500

    feat: add postgres db
    setting new conection to postgresDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,37 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "./auth/auth.module";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { UsersModule } from './users/users.module';
-import { FavoriteModule } from './favorite/favorite.module';
+import { UsersModule } from "./users/users.module";
+import { FavoriteModule } from "./favorite/favorite.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [AuthModule,
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3307,
-      username: 'jeuzmaldo',
-      password: 'root',
-      database: 'inlaze-backend',
+      type: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(`${process.env.POSTGRES_PORT}`),
+      username: process.env.POSTGRES_USERNAME,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
       autoLoadEntities: true, //,
       synchronize: true,
+      ssl: process.env.POSTGRES_SSL === "true",
+      extra: {
+        ssl:
+          process.env.POSTGRES_SSL === "true"
+            ? {
+                rejectUnauthorized: false,
+              }
+            : null,
+      },
     }),
     UsersModule,
-    FavoriteModule
+    FavoriteModule,
   ],
   controllers: [],
   providers: [],
